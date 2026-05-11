a06d656b57e20d63d071cae368bec0419553a6ef
Mariana Garcia <mejiama16@gmail.com>
2026-05-11 09:13:45 -0400

    Uploaded files

---
+++ Donaldson/scripts/03-clean_data.R
@@ -0,0 +1,36 @@
+#### Preamble ####
+# Purpose: Cleans the raw data obtained from OpenData Toronto.
+# Author: Mariana Garcia Mejia
+# Date: 08 May 2026.
+# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# License: MIT
+# Pre-requisites: Package `tidyverse` must be installed.
+
+#### Workspace setup ####
+library(tidyverse)
+library(lubridate)
+
+#### Clean data ####
+raw_data <- read_csv("data/01-raw_data/raw_data.csv")
+
+cleaned_data <-
+  raw_data |>
+  select(Event_Unique_Id, Occurrence_year, Occurrence_Date, 
+         Division, Homicide_Type, Neighbourhood) |>
+  filter(Occurrence_year >= 2011) |> 
+  mutate(Occurrence_Month = month(ymd(Occurrence_Date), label=TRUE)) |>
+  rename(
+    ID = Event_Unique_Id,
+    YEAR = Occurrence_year,
+    DATE = Occurrence_Date,
+    DIVISION = Division,
+    HOMICIDE_TYPE = Homicide_Type,
+    NEIGHBORHOOD = Neighbourhood,
+    MONTH = Occurrence_Month
+  )
+
+
+glimpse(cleaned_data)
+
+#### Save data ####
+write_csv(cleaned_data, "data/02-analysis_data/analysis_data.csv")
